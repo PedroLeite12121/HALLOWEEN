7b3bc12417d005d0001e6cfc6cd9944ad8dc5009
GustavoCarbono <gustavo.s.c.820@gmail.com>
2024-08-15 09:46:59 -0300

    coloquei comentários no java e html

--- paginasInternas/habitos/js/java.js
+++ paginasInternas/habitos/js/java.js
@@ -1,14 +1,15 @@
+// pegar os id lá no html e colocar em uma váriavel no java para ele reconhecer
 var sectionExt = document.getElementById('sectionExt')
 var sectionInt = document.getElementById('sectionInt')
 var text = document.getElementById('text')
 var retornar = document.getElementById('retornar')
 
-function fsection(elemento) {
-    if(elemento == 'fantasia') {
-        sectionExt.style.display = 'none'
-        sectionInt.style.display = 'flex'
-        text.innerText = 'Hábitos e Costumes - Fantasias'
-        sectionInt.innerHTML = '<section id="sectionInt" class="sectionInt">\n<img id="img" src="./habitosIMG/fantasias.png">\n<p>um texto muito aleatorio</p>\n</section>'
+function section(elemento/* pegar aquele texto nas tag <a> dentro dos parênteses e manda aqui como váriavel elemento */) {
+    if(elemento == 'fantasia'/* compara o elemento se é igual a um texto */) {
+        sectionExt.style.display = 'none' // some a section inicial
+        sectionInt.style.display = 'flex' // mostra a outra section
+        text.innerText = 'Hábitos e Costumes - Fantasias' // mudar o header "se quiser mudar no html mude aqui"
+        sectionInt.innerHTML = '<section id="sectionInt" class="sectionInt">\n<img id="img" src="./habitosIMG/fantasias.png">\n<p>um texto muito aleatorio</p>\n</section>' // editar a section secundária como html
         retornar.innerHTML = '<a href="javascript:voltar()" id="retornar">voltar</a>'
     } else if(elemento == 'historia') {
         sectionExt.style.display = 'none'
@@ -37,8 +38,10 @@ function fsection(elemento) {
     }
 }
 
+// volta par a section principal
 function voltar() {
     sectionExt.style.display = 'flex'
     sectionInt.style.display = 'none'
     retornar.innerHTML = '<a href="../../index.html" id="retornar">retornar</a>'
+    text.innerText = 'Halloween - Hábitos e Costumes' // voltar com header
 }
